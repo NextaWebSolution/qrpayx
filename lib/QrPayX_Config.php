cb81349b9b77c163986c260017db73698336416f
Nexta Web Solution <113699283+NextaWebSolution@users.noreply.github.com>
2023-08-01 22:52:10 +0530

    Create QrPayX_Config.php

---
+++ lib/QrPayX_Config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(0);
+$token = ''; // Your Token, (Url:http://example.com/Settings).
+$secret = ''; // Your Secret Key, (Url:http://example.com/Settings).
+$AHKWEB_ENVIRONMENT = 'PROD'; // PROD, TEST
+$callback_url = "https://example.com/QrPayX_kitV2/txnResult.php"; // Enter callback Url where you want to redirect to verify Payment status after payment Done.
+$upiuid = '' // Enter your Paytm business upi id with small latter as it is.
+?>
